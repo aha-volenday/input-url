78865889881977b32ea90c19c215bdd3f4d9920e
Jerome Miranda <jerome.m.miranda@gmail.com>
2019-09-17 11:25:21 +0800

    v0.1.3

--- index.js
+++ index.js
@@ -6,38 +6,14 @@ import { Button, Form, Input, Popover } from 'antd';
 import './styles.css';
 
 export default class InputUrl extends Component {
-	initialState = { errors: [], hasChange: false, isPopoverVisible: false, localValue: '', isFocused: false };
-	state = { ...this.initialState, initialState: this.initialState };
-
-	static getDerivedStateFromProps(nextProps, prevState) {
-		// Set initial localValue
-		if (nextProps.value && !prevState.localValue) {
-			return { ...prevState, localValue: nextProps.value };
-		}
-
-		// Resets equivalent value
-		if (prevState.localValue !== nextProps.value) {
-			// For Add
-			if (typeof nextProps.value === 'undefined' && !prevState.hasChange && !prevState.isFocused) {
-				return { ...prevState.initialState };
-			}
-
-			// For Edit
-			if (!prevState.isFocused) {
-				return { ...prevState.initialState, localValue: nextProps.value };
-			}
-		}
-
-		return null;
-	}
+	state = { errors: [], hasChange: false, isPopoverVisible: false };
 
 	onChange = async value => {
-		const { localValue } = this.state;
 		const { action, id, onChange, onValidate } = this.props;
 
-		if (localValue != '' && value == '') onChange(id, value);
+		onChange(id, value);
 		const errors = this.validate(value);
-		await this.setState({ errors, localValue: value, hasChange: action === 'add' ? false : true });
+		await this.setState({ errors, hasChange: action === 'add' ? false : true });
 		if (onValidate) onValidate(id, errors);
 	};
 
@@ -62,14 +38,12 @@ export default class InputUrl extends Component {
 	};
 
 	renderInput() {
-		const { localValue = '' } = this.state;
 		const {
 			disabled = false,
 			id,
 			label = '',
-			onBlur,
-			onChange,
-			onPressEnter,
+			onBlur = () => {},
+			onPressEnter = () => {},
 			placeholder = '',
 			styles = {},
 			value = ''
@@ -84,19 +58,10 @@ export default class InputUrl extends Component {
 				placeholder={placeholder || label || id}
 				style={styles}
 				type="text"
-				onBlur={e => {
-					if (e.target.value != value) onChange(id, e.target.value);
-					this.setState({ isFocused: false });
-					if (onBlur) onBlur(e);
-				}}
+				onBlur={onBlur}
 				onChange={e => this.onChange(e.target.value)}
-				onFocus={() => this.setState({ isFocused: true })}
-				onPressEnter={e => {
-					onChange(id, e.target.value);
-					if (onPressEnter) onPressEnter(e);
-					return true;
-				}}
-				value={localValue ? localValue : ''}
+				onPressEnter={onPressEnter}
+				value={value ? value : ''}
 			/>
 		);
 	}
