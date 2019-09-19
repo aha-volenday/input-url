3172a24446a4fb29f10ff0ad4c77825a847198d4
Jerome Miranda <jerome.m.miranda@gmail.com>
2019-09-19 11:47:59 +0800

    v0.1.6

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ import './styles.css';
 export default class InputUrl extends Component {
 	state = { errors: [] };
 
+	onChangeTimeout = null;
 	onChange = async (e, value) => {
 		const { id, onChange, onValidate } = this.props;
 
 		onChange(e, id, value);
-		const errors = this.validate(value);
-		await this.setState({ errors });
-		if (onValidate) onValidate(id, errors);
+
+		this.onChangeTimeout && clearTimeout(this.onChangeTimeout);
+		this.onChangeTimeout = setTimeout(async () => {
+			const errors = this.validate(value);
+			await this.setState({ errors });
+			if (onValidate) onValidate(id, errors);
+		}, 500);
 	};
 
 	validate = value => {
